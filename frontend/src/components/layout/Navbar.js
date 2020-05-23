7f39c1a19b47c3cfce172103b6ac26ec7d454cca
jaehyeon48 <imjaehyeon48@gmail.com>
2020-05-23 18:32:37 +0900

    navbar side menu v1

--- frontend/src/components/layout/Navbar.js
+++ frontend/src/components/layout/Navbar.js
@@ -1,26 +1,55 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { Link, withRouter } from 'react-router-dom';
 
 import tyrosLogo from '../../images/tyros-logo.png';
 import './Navbar.css';
 
-const Navbar = () => {
+const Navbar = ({
+  history
+}) => {
+  const [isSideOpen, setIsSideOpen] = useState(false);
+
+  const handleSideMenu = () => {
+    setIsSideOpen(!isSideOpen);
+  };
+
+  const linkToLogin = () => {
+    history.push('/login');
+    setIsSideOpen(!isSideOpen);
+  }
+
+  const linkToSignUp = () => {
+    history.push('/signup');
+    setIsSideOpen(!isSideOpen);
+  };
   return (
-    <nav className="navbar py-half">
-      <div className="navbar-logo">
-        <Link to="/">
-          <img src={tyrosLogo} alt="tyros main logo" />
-        </Link>
+    <React.Fragment>
+      <div className="navbar-container">
+        <nav className="navbar py-half">
+          <div className="navbar-logo">
+            <Link to="/">
+              <img src={tyrosLogo} alt="tyros main logo" />
+            </Link>
+          </div>
+          <div className="navbar-auth-links">
+            <Link to="/login" className="navbar-login">Login</Link>
+            <Link to="/signup" className="navbar-signUp">Sign Up</Link>
+          </div>
+          <div className="navbar-bars" onClick={handleSideMenu}>
+            {isSideOpen ? (<i class="fas fa-times"></i>) : (<i class="fas fa-bars"></i>)}
+          </div>
+        </nav>
+        <div className={isSideOpen ? "navbar-sideMenu" : "navbar-sideMenu-hidden"}>
+          <div className="side-login" onClick={linkToLogin}>
+            <Link to="/login">Login</Link>
+          </div>
+          <div className="side-signUp" onClick={linkToSignUp}>
+            <Link to="/signup">Sign Up</Link>
+          </div>
+        </div>
       </div>
-      <div className="navbar-auth-links">
-        <Link to="/login" className="navbar-login">Login</Link>
-        <Link to="/signup" className="navbar-signUp">Sign Up</Link>
-      </div>
-      <div className="navbar-bars">
-        <i class="fas fa-bars"></i>
-      </div>
-    </nav>
+    </React.Fragment>
   );
 };
 
-export default Navbar;
+export default withRouter(Navbar);
