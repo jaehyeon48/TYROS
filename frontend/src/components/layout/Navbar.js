6361fe90795a44f53591cb78aab803c3b455b37a
jaehyeon48 <imjaehyeon48@gmail.com>
2020-05-23 16:46:16 +0900

    responsive landing v1

--- frontend/src/components/layout/Navbar.js
+++ frontend/src/components/layout/Navbar.js
@@ -8,7 +8,9 @@ const Navbar = () => {
   return (
     <nav className="navbar py-half">
       <div className="navbar-logo">
-        <img src={tyrosLogo} alt="tyros main logo" />
+        <Link to="/">
+          <img src={tyrosLogo} alt="tyros main logo" />
+        </Link>
       </div>
       <div className="navbar-auth-links">
         <Link to="/login" className="navbar-login">Login</Link>
