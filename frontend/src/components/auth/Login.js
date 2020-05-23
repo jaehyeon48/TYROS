3b61323982eb484f1a76ce9abb7fb17d2d33764b
jaehyeon48 <imjaehyeon48@gmail.com>
2020-05-23 23:20:10 +0900

    login form v1

--- frontend/src/components/auth/Login.js
+++ frontend/src/components/auth/Login.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import './Login.css';
@@ -7,7 +8,34 @@ const Login = props => {
   return (
     <React.Fragment>
       <div className="container">
+        <p className="login-subtitle">Join TYROS</p>
         <h1 className="text-main login-title">LOGIN</h1>
+        <form className="form-login">
+          <div className="form-group group-login-email">
+            <label className="form-label">Email</label>
+            <input
+              type="email"
+              className="form-field"
+              name="email"
+              placeholder="Email"
+            />
+          </div>
+          <div className="form-group group-login-password">
+            <label className="form-label">Password</label>
+            <input
+              type="password"
+              className="form-field"
+              name="password"
+              placeholder="Password"
+            />
+          </div>
+          <div className="form-group group-btn-login">
+            <button className="btn btn-login">LOGIN</button>
+          </div>
+        </form>
+        <div className="have-account">
+          Don't have an account? <Link to="/signup">Sign Up</Link>
+        </div>
       </div>
     </React.Fragment>
   );
--- frontend/src/components/auth/SignUp.js
+++ frontend/src/components/auth/SignUp.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 
 import './SignUp.css';
@@ -10,7 +11,7 @@ const SignUp = props => {
         <p className="signup-subtitle">Join TYROS</p>
         <h1 className="signup-title">CREATE YOUR ACCOUNT</h1>
         <form className="form-signUp">
-          <div className="form-group group-firstName">
+          <div className="form-group group-signUp-firstName">
             <label className="form-label">First Name</label>
             <input
               type="text"
@@ -19,7 +20,7 @@ const SignUp = props => {
               placeholder="First Name"
             />
           </div>
-          <div className="form-group group-lastName">
+          <div className="form-group group-signUp-lastName">
             <label className="form-label">Last Name</label>
             <input
               type="text"
@@ -28,7 +29,7 @@ const SignUp = props => {
               placeholder="Last Name"
             />
           </div>
-          <div className="form-group group-email">
+          <div className="form-group group-signUp-email">
             <label className="form-label">Email</label>
             <input
               type="email"
@@ -37,7 +38,7 @@ const SignUp = props => {
               placeholder="Email"
             />
           </div>
-          <div className="form-group group-password">
+          <div className="form-group group-signUp-password">
             <label className="form-label">Password</label>
             <input
               type="password"
@@ -50,6 +51,9 @@ const SignUp = props => {
             <button className="btn btn-signUp">SIGN UP</button>
           </div>
         </form>
+        <div className="have-account">
+          Already have an account? <Link to="/login">Log in</Link>
+        </div>
       </div>
     </React.Fragment>
   );
