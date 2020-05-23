01bcee3c9ef9451377d83399935b5cefd0d2853d
jaehyeon48 <imjaehyeon48@gmail.com>
2020-05-23 21:26:11 +0900

    sign up form v1

--- frontend/src/components/auth/SignUp.js
+++ frontend/src/components/auth/SignUp.js
@@ -9,6 +9,47 @@ const SignUp = props => {
       <div className="container">
         <p className="signup-subtitle">Join TYROS</p>
         <h1 className="signup-title">CREATE YOUR ACCOUNT</h1>
+        <form className="form-signUp">
+          <div className="form-group group-firstName">
+            <label className="form-label">First Name</label>
+            <input
+              type="text"
+              className="form-field"
+              name="firstName"
+              placeholder="First Name"
+            />
+          </div>
+          <div className="form-group group-lastName">
+            <label className="form-label">Last Name</label>
+            <input
+              type="text"
+              className="form-field"
+              name="lastName"
+              placeholder="Last Name"
+            />
+          </div>
+          <div className="form-group group-email">
+            <label className="form-label">Email</label>
+            <input
+              type="email"
+              className="form-field"
+              name="email"
+              placeholder="Email"
+            />
+          </div>
+          <div className="form-group group-password">
+            <label className="form-label">Password</label>
+            <input
+              type="password"
+              className="form-field"
+              name="password"
+              placeholder="Password"
+            />
+          </div>
+          <div className="form-group group-btn-signUp">
+            <button className="btn btn-signUp">SIGN UP</button>
+          </div>
+        </form>
       </div>
     </React.Fragment>
   );
